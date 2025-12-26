3f19df07319ca5665d360e79e4b647182d51c8c9
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2025-12-26 10:05:07 -0300

    nodemailer config

--- apps/notification-service/src/app/app.module.ts
+++ apps/notification-service/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import path from 'path';
+import { MailerModule } from '@nestjs-modules/mailer';
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: [
@@ -10,7 +11,24 @@ import path from 'path';
       path.resolve(process.cwd(), 'apps/notification-service/.env')
     ],
     isGlobal: true
-  }),],
+  }),MailerModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory:(configService: ConfigService) => ({
+      transport: {
+        host: configService.get<string>('MAIL_HOST'),
+        port: configService.get<number>('MAIL_PORT'),
+        secure: configService.get<boolean>('MAIL_SECURE'),
+        auth: {
+          user: configService.get<string>('MAIL_USER'),
+          pass: configService.get<string>('MAIL_PASS'),
+        },
+      },
+      defaults: {
+        from: `"No Reply" <${configService.get<string>('MAIL_FROM')}>`,
+      },
+    }),
+    inject: [ConfigService],
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
